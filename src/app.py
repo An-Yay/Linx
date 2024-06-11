10fab9de767b39e1c37613692671abcc02b15d19
Ananay <ananaygupta23@gmail.com>
2024-06-11 22:24:20 +0530

    downloading and showing html on the terminal

--- src/app.py
+++ src/app.py
@@ -1,64 +1,68 @@
 import socket
+import ssl
+
 
 class URL:
     def __init__(self, url):
-        self.scheme, url = url.split("://",1)
-        assert self.scheme=="http"
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme == "http"
 
         if "/" not in url:
-            url=url+"/"
+            url = url + "/"
 
-        self.host, url = url.split("/",1)
-        self.path = "/"+url
-    
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+
     def request(self):
-        
-        sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP,)
-
-        sock.connect((self.host,80))
-        request="GET {} HTTP/1.0\r\n".format(self.path)
-        request+="HOST: {}\r\n".format(self.host)
-        request+="\r\n"
+        sock = socket.socket(family=socket.AF_INET,
+                             type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
+        sock.connect((self.host, 80))
+        request = "GET {} HTTP/1.0\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "\r\n"
 
         sock.send(request.encode("utf8"))
 
         response = sock.makefile("r", encoding="utf8", newline="\r\n")
 
         # Status line
-        statusLine = response.readline
-        version, status, explanation = statusLine.split(" ",2)
+        statusLine = response.readline()
+        version, status, explanation = statusLine.split(" ", 2)
 
-        #Headers
-        response_headers ={}
+        # Headers
+        response_headers = {}
         while True:
             line = response.readline()
             if line == "\r\n":
                 break
-            
-            header, value = line.split(":",1)
+
+            header, value = line.split(":", 1)
             response_headers[header.casefold()] = value.strip()
 
-            assert "content-encoding" not in response_headers
-            assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+        assert "transfer-encoding" not in response_headers
 
-            content = response.read()
-            sock.close
-
-            return content
-
+        content = response.read()
+        sock.close()
+        return content
 
 
+def show(body):
+    in_tag = False
+    for c in body:
+        if c == '<':
+            in_tag = True
+        elif c == '>':
+            in_tag = False
+        elif not in_tag:
+            print(c, end=" ")
 
 
+def load(url):
+    body = url.request()
+    show(body)
 
 
-
-
-
-
-
-url=URL("http://example.com/abc.html")
-print("The url scheme is: ",url.scheme)
-print("The host is: ",url.host)
-print("The path is: ",url.path)
-
+if __name__ == "__main__":
+    import sys
+    load(URL(sys.argv[1]))
